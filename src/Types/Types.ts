6498628d1a16bf5a99463d51b8d6ea719e20389d
SaghiBzd <saghi.bzd22@gmail.com>
2024-12-16 15:18:30 +0330

    Update Types.ts to be used in Products.tsx and ProductCard.tsx

--- src/Types/Types.ts
+++ src/Types/Types.ts
@@ -1,3 +1,12 @@
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  description: string;
+  image: string;
+}
+
 export interface GeoLocation {
   lat: string;
   long: string;
