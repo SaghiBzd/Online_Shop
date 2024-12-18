6435fd7afd64f7bd02f7a9b58e72c004018f3862
SaghiBzd <saghi.bzd22@gmail.com>
2024-12-18 12:34:34 +0330

    Update Products.tsx according to the Loading components

--- src/Components/Products.tsx
+++ src/Components/Products.tsx
@@ -3,12 +3,13 @@ import { useCart } from '../Store/CartContext';
 import { Product } from '../Types/Type';
 import SearchAndFilter from './Search&Filter';
 import ProductCard from './ProductCard';
+import Loading from './Loading';
 
 const Products = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [categoryFilter, setCategoryFilter] = useState('');
-  const [isLoading, setIsLoading] = useState(true); // Add isLoading state
+  const [isLoading, setIsLoading] = useState(true);
   const { addToCart } = useCart();
 
   useEffect(() => {
@@ -16,12 +17,11 @@ const Products = () => {
       .then((res) => res.json())
       .then((data) => {
         setProducts(data);
-        setIsLoading(false); 
+        setIsLoading(false);
       })
-      .then(json=>console.log(json))
       .catch(() => setIsLoading(false));
-    }, []);
-  
+  }, []);
+
   const filteredProducts = useMemo<Product[]>(() => {
     return products.filter((product) => {
       return (
@@ -34,6 +34,7 @@ const Products = () => {
   const handleAddToCart = useCallback((product: Product) => {
     addToCart(product);
   }, [addToCart]);
+
   return (
     <div className="my-6 px-4 sm:px-6 lg:px-8">
       <h1 className="text-4xl font-bold mb-4">Products</h1>
@@ -45,12 +46,7 @@ const Products = () => {
       />
       <div className="px-9 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
         {isLoading ? (
-          <div className="col-span-full text-center py-10">
-            <p className="text-lg font-semibold">Loading products...</p>
-            <div className="mt-4 flex justify-center items-center space-x-2">
-              <div className="w-5 h-5 border-4 border-t-transparent border-solid rounded-full animate-spin"></div>
-            </div>
-          </div>
+          <Loading />
         ) : filteredProducts.length > 0 ? (
           filteredProducts.map((product) => (
             <ProductCard
@@ -62,7 +58,8 @@ const Products = () => {
         ) : (
           <div className="col-span-full text-center py-10">
             <p className="text-lg font-semibold">
-              Sorry, we do not have the product you searched for!</p>
+              Sorry, we do not have the product you searched for!
+            </p>
           </div>
         )}
       </div>
