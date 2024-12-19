bc31b3e65063d7bf575137d97a8f924cc3765ad0
SaghiBzd <saghi.bzd22@gmail.com>
2024-12-19 21:06:50 +0330

    Update RootLayout.tsx

--- src/Components/RootLayout.tsx
+++ src/Components/RootLayout.tsx
@@ -1,13 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { useCart } from "../Store/CartContext";
 
 const RootLayout: React.FC = () => {
-    return <>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
+  const { cart } = useCart();
+
+  const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0);
+
+  return (
+    <>
+      <Navbar totalQuantity={totalQuantity} />
+      <Outlet />
+      <Footer />
     </>
+  );
 };
 
 export default RootLayout;
